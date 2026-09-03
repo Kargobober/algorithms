b8a9469b33b668939b15f2f014f564ccb42e91ec
Kargobober <kargobober@yandex.ru>
2026-09-03 21:56:52 +0300

    twoSum

--- src/algorithms/numbers.ts
+++ src/algorithms/numbers.ts
@@ -10,3 +10,46 @@ function getSum(arr: number[]): number {
   }
   return sum;
 }
+
+// Найти индексы чисел, дающих в сумме target. Решение во входных данных всегда есть и оно одно.
+function twoSum_v1(nums: number[], target: number): number[] {
+  const maper = nums.map((num, idx) => ({ num, idx }));
+
+  maper.sort((a, b) => a.num - b.num);
+
+  let l = 0;
+  let r = maper.length - 1;
+
+  while (l < r) {
+    const sum = maper[l]!.num + maper[r]!.num;
+
+    if (sum > target) {
+      r--;
+    } else if (sum < target) {
+      l++;
+    } else {
+      return [maper[l]!.idx, maper[r]!.idx];
+    }
+  }
+
+  return [];
+}
+
+function twoSum_v2(nums: number[], target: number): number[] {
+  // number value --> index
+  const mapa = new Map<number, number>();
+
+  mapa.set(nums[0]!, 0);
+
+  for (let i = 1; i < nums.length; i++) {
+    const complement = target - nums[i]!;
+
+    if (mapa.has(complement)) {
+      return [mapa.get(complement)!, i];
+    } else {
+      mapa.set(nums[i]!, i);
+    }
+  }
+
+  return [];
+}
